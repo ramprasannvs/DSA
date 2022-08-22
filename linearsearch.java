a49814b869c79926401e5965f79f4bb01a7009da
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-22 13:09:46 +0530

    Linear search

---
+++ linearsearch.java
@@ -0,0 +1,13 @@
+public class linearsearch {
+    public static void binary(int num[],int key){
+        int start=0,end=num.length-1;
+        System.out.println(end);
+        int mid=(start+end)/2;
+        System.out.println(mid);
+    }
+  public static void main(String[] args) {
+    int number[]={2,4,6,8,10,12,14};
+    int key=10;
+    binary(number, key);
+  }  
+}
