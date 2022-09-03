e00c04853db76d58b0227aa7d11dbb29dd6c968e
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-09-03 23:08:38 +0530

    inbuilt.java

---
+++ inbuiltsort.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+public class inbuiltsort {
+    public static void main(String[] args) {
+        int arr[]={5,7,3,2,1};
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    
+}
+
